e3643c461abbd96e134fe095e0218dba37260b82
Viacheslav Pryimak <vpryimak@intropro.com>
2016-03-30 12:14:38 +0300

    fix errors

--- src/usr/local/sbin/start.sh
+++ src/usr/local/sbin/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-export ELASTICSEARCH_PORT_9200_TCP_ADDR=${ELASTICSEARCH_PORT_9200_TCP_ADDR}
-
-sed -i "s/localhost/elasticsearch.default.svc.cluster.local/g" /etc/logstash/conf.d/java.conf
+#export ELASTICSEARCH_PORT_9200_TCP_ADDR=${ELASTICSEARCH_PORT_9200_TCP_ADDR}
+#3sed -i "s/localhost/elasticsearch.default.svc.cluster.local/g" /etc/logstash/conf.d/java.conf
 
 /opt/logstash/bin/logstash agent --config /etc/logstash/conf.d/java.conf
