16ea759d324e512470cc110289ddd4b5b7daac69
Viacheslav Pryimak <vpryimak@intropro.com>
2016-03-30 14:22:12 +0300

    add log file

--- src/usr/local/sbin/start.sh
+++ src/usr/local/sbin/start.sh
@@ -3,4 +3,4 @@
 #export ELASTICSEARCH_PORT_9200_TCP_ADDR=${ELASTICSEARCH_PORT_9200_TCP_ADDR}
 #3sed -i "s/localhost/elasticsearch.default.svc.cluster.local/g" /etc/logstash/conf.d/java.conf
 
-/opt/logstash/bin/logstash agent --config /etc/logstash/conf.d/java.conf
+/opt/logstash/bin/logstash agent --config /etc/logstash/conf.d/java.conf --log /var/log/logstash/logstash.log
